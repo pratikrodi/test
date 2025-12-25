dd9f11fc4d9374082de1129380dd0958815ada50
Pratik Rodi <133090604+pratikrodi@users.noreply.github.com>
2025-12-25 22:07:59 +0530

    Fix syntax errors in terraform_file.tf

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,58 +1,22 @@
 provider "aws" {
-
-2
-
 region "us-east-1"
-
-3
-
 }
 
-4
+resource "aws_instance" "myinstance" {
 
-2
-
-5
-
-le
-
-6
-
-md
-
-7
-
-U
-
-8
-
-9
-
-10
-
-resource "aws_instance" "myinstance" T
-
-ami "ami-0c398cb65a93047f2"
+ami = "ami-0c398cb65a93047f2"
 
 instance_type = "t3.micro"
 
-vpc_security_group_ids= ["sg-0f7b2e9cf35734448"]
+vpc_security_group_ids = ["sg-0f7b2e9cf35734448"]
 
-key_name "id_rsa"
+key_name = "id_rsa"
 
-availability_zone "us-east-1a"
-
-11
+availability_zone = "us-east-1a"
 
 tags = {
 
-12
-
-name
-
-"myinstance"
-
-13
+name = "myinstance"
 
 }
 }
